8c95643cd32340206c58e5aedb7c70dce8cea24c
Anixias <colnusca@gmail.com>
2022-07-31 20:07:12 -0500

    Added Label to ImageFieldEditor
    - Added Label to ImageFieldEditor

--- ImageFieldEditor.cs
+++ ImageFieldEditor.cs
@@ -5,6 +5,7 @@ public class ImageFieldEditor : FieldEditor
 {
 	private TextureRect image;
 	private TextInputBox textInputBox;
+	private Label label;
 	
 	public new ImageField Field
 	{
@@ -40,6 +41,7 @@ public class ImageFieldEditor : FieldEditor
 	
 	public override void _Ready()
 	{
+		label = GetNode<Label>("VBoxContainer/Label");
 		image = GetNode<TextureRect>("VBoxContainer/Image");
 		textInputBox = GetNode<TextInputBox>("VBoxContainer/Path/TextInputBox");
 		
@@ -65,6 +67,13 @@ public class ImageFieldEditor : FieldEditor
 	public override void UpdateState()
 	{
 		var editable = (!Inherited || Overriding);
+		
+		if (label != null)
+		{
+			label.Text = field?.Name ?? "";
+			var color = overriding ? new Color(0.11f, 1.0f, 0.61f, 1.0f) : new Color(1.0f, 1.0f, 1.0f, 1.0f);
+			label.Modulate = editable ? color : new Color(1.0f, 1.0f, 1.0f, 0.6f);
+		}
 	}
 	
 	public void OnBrowseButtonPressed()
