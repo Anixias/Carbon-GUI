c3e4ddea580974448424f0af7cfc4fd2d137e8f1
Anixias <colnusca@gmail.com>
2022-08-02 01:14:20 -0500

    Restored whitespace in Interface.cs
    - Restored whitespace in Interface.cs that was automatically trimmed accidentally

--- Interface.cs
+++ Interface.cs
@@ -11,83 +11,83 @@ public class Interface : Control
 			target?.Enter();
 			return target;
 		}
-
+		
 		public static ApplicationState Initialize(ApplicationState target)
 		{
 			target?.Enter();
 			return target;
 		}
-
+		
 		public abstract void Enter();
 		public abstract void Exit();
 	}
-
+	
 	private class SplashScreenState : ApplicationState
 	{
 		private Control splashScreen;
-
+		
 		public SplashScreenState(Control splashScreen)
 		{
 			this.splashScreen = splashScreen;
 		}
-
+		
 		public override void Enter()
 		{
 			splashScreen.Visible = true;
 		}
-
+		
 		public override void Exit()
 		{
 			splashScreen.Visible = false;
 		}
 	}
-
+	
 	private class ProjectEditorState : ApplicationState
 	{
 		private Project project;
 		private ProjectEditor projectEditor;
-
+		
 		public ProjectEditorState(Project project, ProjectEditor projectEditor)
 		{
 			this.project = project;
 			this.projectEditor = projectEditor;
 		}
-
+		
 		public override void Enter()
 		{
 			projectEditor.SetProject(project);
 			projectEditor.Visible = true;
 		}
-
+		
 		public override void Exit()
 		{
 			projectEditor.Visible = false;
 			projectEditor.SetProject(null);
 		}
 	}
-
+	
 	private ApplicationState state;
 	private CenterContainer splashScreen;
 	private ProjectEditor projectEditor;
 	private MenuBar menuBar;
-
+	
 	public override void _Ready()
 	{
 		OS.WindowMaximized = true;
-
+		
 		splashScreen = GetNode<CenterContainer>("MarginContainer/VBoxContainer/SplashScreen");
 		projectEditor = GetNode<ProjectEditor>("MarginContainer/VBoxContainer/ProjectEditor");
 		menuBar = GetNode<HBoxContainer>("MarginContainer/VBoxContainer/MenuBar") as MenuBar;
-
+		
 		menuBar.Connect(nameof(MenuBar.Undo), projectEditor, nameof(ProjectEditor.Undo));
 		menuBar.Connect(nameof(MenuBar.Redo), projectEditor, nameof(ProjectEditor.Redo));
 		projectEditor.Connect(nameof(ProjectEditor.UpdateProjectStatus), menuBar, nameof(MenuBar.SetProjectOpened));
 		projectEditor.Connect(nameof(ProjectEditor.UpdateUndo), menuBar, nameof(MenuBar.SetUndoEnabled));
 		projectEditor.Connect(nameof(ProjectEditor.UpdateRedo), menuBar, nameof(MenuBar.SetRedoEnabled));
-
+		
 		state = ApplicationState.Initialize(new SplashScreenState(splashScreen));
 	}
-
+	
 	public override void _Notification(int notification)
 	{
 		switch(notification)
@@ -99,7 +99,7 @@ public class Interface : Control
 				break;
 		}
 	}
-
+	
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event is InputEventMouseButton mbEvent)
@@ -109,7 +109,7 @@ public class Interface : Control
 				GetFocusOwner()?.ReleaseFocus();
 			}
 		}
-
+		
 		if (@event is InputEventKey keyEvent)
 		{
 			if (keyEvent.GetScancodeWithModifiers() == (uint)KeyList.Escape)
@@ -118,7 +118,7 @@ public class Interface : Control
 			}
 		}
 	}
-
+	
 	public void OnNewProject()
 	{
 		var project = new Project();
@@ -127,7 +127,7 @@ public class Interface : Control
 			state = state.Transition(new ProjectEditorState(project, projectEditor));
 		}
 	}
-
+	
 	public void OnCloseProject()
 	{
 		state = state.Transition(new SplashScreenState(splashScreen));
